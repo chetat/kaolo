39ca6f03bb709db005a09f3903c5a27b12dbfd0d
yeku wilfred <yekuwilfred@gmail.com>
2023-05-19 23:49:55 +0100

    refact: add logger

---
+++ app/logger/kaolog.py
@@ -0,0 +1,25 @@
+import logging
+import json
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_object = {
+            'level': record.levelname,
+            'message': record.getMessage(),
+            'time': self.formatTime(record),
+            'source': record.pathname,
+            'line': record.lineno,
+            'function': record.funcName
+        }
+        return json.dumps(log_object)
+
+
+def get_logger(name):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(JsonFormatter())
+
+    logger.addHandler(handler)
+    return logger
